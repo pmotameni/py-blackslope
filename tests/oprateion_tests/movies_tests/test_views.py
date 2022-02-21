abbd5d7bf1ec09c519e36919343ad67dec960723
pmotameni <47757728+pmotameni@users.noreply.github.com>
2022-02-20 22:21:27 -0600

    testing get movies

--- tests/oprateion_tests/movies_tests/test_views.py
+++ tests/oprateion_tests/movies_tests/test_views.py
@@ -1,5 +1,20 @@
 from apiapp.operations.movies import MoviesController
+from apiapp.services.movies import Movie as MovieDomainModel
+import pytest
+from rest_framework.test import APIRequestFactory
 
 
-def test_get():
-    controller = MoviesController()
+@pytest.fixture(scope="function")
+def factory():
+    return APIRequestFactory()
+
+
+def test_single(factory, mocker):
+    return_value = [MovieDomainModel(id=1, title="test movie")]
+    mocker.patch("apiapp.services.movies.MovieService.get_movies",
+                 return_value=return_value)
+    request = factory.get("/movies/", format='json')
+    view = MoviesController.as_view()
+    response = view(request)
+    assert response.status_code == 200
+    assert response.content == b'{"movies": [{"id": "1", "title": "test movie", "description": null, "release_date": null}]}'
